9fc4285a97561ac3cd8df668d6be94320a90dd49
Ajay paul <falcon0521@gmail.com>
2024-09-02 14:33:39 +0530

    g

---
+++ App.js
@@ -0,0 +1,26 @@
+
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, Text, View } from 'react-native';
+const Stack = createStackNavigator();
+
+export default function App() {
+  return (
+    
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Splash" screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="notification" component={Notification} screenOptions={{ headerShown: false }} />
+      </Stack.Navigator>
+      </NavigationContainer>
+
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
